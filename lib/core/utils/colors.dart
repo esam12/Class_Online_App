dfd35377fc82b0539da364a275e5fd96a12875c2
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-10-30 22:53:16 +0300

    This commit initializes a new project and adds Firebase integration. It includes setting up the Firebase project, configuring the Firebase SDK, and initializing the Firebase services. This commit also adds some initial color styling to the project.
    - Initialized new project
    - Added Firebase integration
    - Configured Firebase SDK
    - Initialized Firebase services
    - Added initial color styling

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/material.dart';
+
+const Color appBarColor = Color(0xffF0F4F7);
+const Color bodyColor = Color(0xffFCFEFD);
+const Color iconBackgroundColor = Color(0xff34353D);
+const Color bottomSheetColor = Color(0xff222222);
